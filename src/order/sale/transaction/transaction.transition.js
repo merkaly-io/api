5d5d7c19c90e880a9d363fd94c62f63bf1e4bb83
Randy Tellez Galan <kronhyx@gmail.com>
2026-06-26 16:14:25 -0300

    refactor: add order types

--- src/order/sale/transaction/transaction.transition.js
+++ src/order/sale/transaction/transaction.transition.js
@@ -3,9 +3,9 @@ import * as transaction_enum_1 from './transaction.enum';
 
 export class SaleTransactionTransition extends AbstractTransition {
   $states = {
-        [transaction_enum_1.SaleTransactionStatusEnum.OPEN]: [transaction_enum_1.SaleTransactionStatusEnum.IN_PROGRESS, transaction_enum_1.SaleTransactionStatusEnum.COMPLETED, transaction_enum_1.SaleTransactionStatusEnum.CANCELLED],
+        [transaction_enum_1.SaleTransactionStatusEnum.OPEN]: [transaction_enum_1.SaleTransactionStatusEnum.IN_PROGRESS, transaction_enum_1.SaleTransactionStatusEnum.CANCELLED],
         [transaction_enum_1.SaleTransactionStatusEnum.IN_PROGRESS]: [transaction_enum_1.SaleTransactionStatusEnum.COMPLETED, transaction_enum_1.SaleTransactionStatusEnum.CANCELLED],
-        [transaction_enum_1.SaleTransactionStatusEnum.COMPLETED]: [],
+        [transaction_enum_1.SaleTransactionStatusEnum.COMPLETED]: [transaction_enum_1.SaleTransactionStatusEnum.CANCELLED],
         [transaction_enum_1.SaleTransactionStatusEnum.CANCELLED]: [],
     };
 
---
+++ src/order/type/order.enum.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OrderTypeEnum = void 0;
+var OrderTypeEnum;
+(function (OrderTypeEnum) {
+    OrderTypeEnum["SALE"] = "SALE";
+    OrderTypeEnum["PURCHASE"] = "PURCHASE";
+})(OrderTypeEnum || (exports.OrderTypeEnum = OrderTypeEnum = {}));
+//# sourceMappingURL=order.enum.js.map
